5ad6f83f221db2f2dc60f65b5e0a3b8c32ccb9d7
sarath.krishnan@slalom.com-at-617780835636 <sarath.krishnan@S01423-MBPR.local>
2020-05-01 12:29:01 -0400

    Updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# terraform-azure-infrastructure
-This repository contains Terraform scripts to build resources in Azure and references modules in other repos.
--- providers.tf
+++
@@ -1,8 +0,0 @@
-#########################################################
-## Azure Provider Version 
-# whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider. 
-#########################################################
-provider "azurerm" {
-  version = "=2.8.0"
-  features {}
-}
